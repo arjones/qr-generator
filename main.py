bb7ffdbbd58d9c6e7bfbcc4576eb17b657c11d86
Gustavo Arjones <gustavo@arjones.net>
2023-03-04 19:47:50 +0100

    Updating default url

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-DEFAULT_URL = "https://arjon.es/"
+DEFAULT_URL = "https://arjon.es/links"
 
 
 @app.get("/", response_class=HTMLResponse)
